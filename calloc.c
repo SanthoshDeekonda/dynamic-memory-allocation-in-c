dced59285649d0fdfed9a6c204c0b533f8c766f2
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-08 13:42:14 +0530

    allocate memory using calloc

--- calloc.c
+++ calloc.c
@@ -8,6 +8,7 @@ int main(){
     printf("\nEnter the number of blocks: ");
     scanf("%d", &n);
 
+
     //allocating 10 blocks of memory of int(4 bytes)
     int* ptr = (int*)calloc(10,sizeof(int));
 
