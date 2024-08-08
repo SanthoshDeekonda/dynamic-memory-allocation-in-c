b622dee3d956b996c86b0b5a2c06b7511a2e5014
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-08 13:37:05 +0530

    implement free

---
+++ free.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    //allocating memory
+    int* ptr = (int*)malloc(4);
+
+    printf("%zu bytes", sizeof(*ptr));
+    *ptr = 10;
+    printf("\n%d", *ptr);
+    
+    free(ptr);
+    
+    printf("\n%d", *ptr);
+
+    return 0;   
+
+}
