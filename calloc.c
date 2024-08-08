6b619da8dd62d8d8966b59ae9f194c1372d3698c
Santhosh Deekonda <deekondasanthosh609@gmail.com>
2024-08-08 13:26:16 +0530

    final memory allocation

---
+++ calloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int n,i;
+
+    printf("\nEnter the number of blocks: ");
+    scanf("%d", &n);
+
+    //allocating 10 blocks of memory of int(4 bytes)
+    int* ptr = (int*)calloc(10,sizeof(int));
+
+    for(i=0;i<n;i++){
+        printf("\n%d -> %p",ptr[i], ptr+i);
+    }
+
+
+    return 0;
+}
